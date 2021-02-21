e7c3a6e8eb08601717fcc2a7d774e101859d6186
Yuriy <mork1cher@gmail.com>
2021-02-21 13:07:36 +0300

    Сделал кривое менб на комп версии

---
+++ js/comp_script_menu.js
@@ -0,0 +1,18 @@
+function Evacuation(){
+    document.getElementById('Evacuation')       .style.display = "block";
+    document.getElementById('Auto_park')        .style.display = "none";
+    document.getElementById('Sto')              .style.display = "none";
+}
+
+function Auto_park(){
+    document.getElementById('Evacuation')       .style.display = "none";
+    document.getElementById('Auto_park')        .style.display = "block";
+    document.getElementById('Sto')              .style.display = "none";
+}
+
+function Sto(){
+    document.getElementById('Evacuation')       .style.display = "none";
+    document.getElementById('Auto_park')        .style.display = "none";
+    document.getElementById('Sto')              .style.display = "block";
+}
+
